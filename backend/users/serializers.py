060c6fecdc7f5fcd49f4e6587f69a20536236e37
Rafael <rafaelfragoso546@gmail.com>
2025-03-25 22:03:19 +0000

    Serializers adaptado

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -8,15 +8,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = get_user_model()
-        fields = [
-            "id",
-            "username",
-            "is_superuser",
-            "is_staff",
-            "email",
-            "last_login",
-            "password"
-        ]
+        fields = ("id", "username", "first_name", "last_name", "is_superuser", "is_staff", "email", "last_login")
 
     def update(self, instance, validated_data):
         password = validated_data.pop("password", None)
@@ -25,3 +17,15 @@ class UserSerializer(serializers.ModelSerializer):
             instance.set_password(password)
             instance.save()
         return instance
+    
+class CustomRegisterSerializer(DefaultRegisterSerializer):
+    first_name = serializers.CharField(required=False)
+    last_name = serializers.CharField(required=False)
+
+    def custom_signup(self, request, user):
+        user.first_name = self.validated_data.get('first_name', '')
+        user.last_name = self.validated_data.get('last_name', '')
+        user.save()
+
+
+
