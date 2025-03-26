4a03a280d50e585d3e94bcea4e40bd90afecadd4
DavidK07442 <a81085@ualg.pt>
2025-03-26 10:59:38 +0000

    Revert "Merge branch 'main' of https://github.com/MarioNogueira01/doccano_"
    This reverts commit 6b350bffe500c22b4955f8c1c8ca92a13541c434, reversing
    changes made to 025be9887491c4ba9ca3a70e095c73b80791eaff.

--- frontend/components/users/UserEditForm.vue
+++ frontend/components/users/UserEditForm.vue
@@ -1,83 +1,95 @@
 <template>
-    <v-card>
-      <v-card-title>
-        <span class="text-h5">Editar Usuário</span>
-      </v-card-title>
-  
-      <v-card-text>
-        <v-form ref="form" v-if="user">
-          <v-text-field
-            v-model="formData.username"
-            label="Username"
-            required
-          />
-          <v-text-field
-            v-model="formData.email"
-            label="Email"
-            type="email"
-            required
-          />
-          <v-text-field
-            v-model="formData.password"
-            label="Nova Senha"
-            type="password"
-          />
-        </v-form>
-      </v-card-text>
-  
-      <v-card-actions>
-        <v-btn color="primary" text @click="onCancel">Cancelar</v-btn>
-        <v-btn color="primary" @click="onSave">Salvar</v-btn>
-      </v-card-actions>
-    </v-card>
-  </template>
-  
-  <script>
-  import userService from '~/services/userService' // Ajuste caminho conforme seu projeto
-  
-  export default {
-    name: 'UserEditForm',
-    props: {
-      user: {
-        type: Object,
-        required: true
+  <v-card>
+    <v-card-title>
+      <span class="text-h5">Editar Usuário</span>
+    </v-card-title>
+
+    <v-card-text>
+      <v-form ref="form" v-if="user">
+        <v-text-field
+          v-model="formData.username"
+          label="Username"
+          required
+        />
+        <v-text-field
+          v-model="formData.email"
+          label="Email"
+          type="email"
+          required
+        />
+        <v-text-field
+          v-model="formData.password"
+          label="Nova Senha"
+          type="password"
+        />
+      </v-form>
+    </v-card-text>
+
+    <v-card-actions>
+      <v-btn color="primary" text @click="onCancel">Cancelar</v-btn>
+      <v-btn color="primary" @click="onSave">Salvar</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+import userService from '~/services/userService'
+
+export default {
+  name: 'UserEditForm',
+  props: {
+    user: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      formData: {
+        id: null,
+        username: '',
+        email: '',
+        password: ''
       }
-    },
-    data() {
-      return {
-        formData: {
-          username: '',
-          email: '',
-          password: ''
-        }
-      }
-    },
-    created() {
-      // Quando o componente é criado, copiamos as infos do user para formData
+    }
+  },
+  watch: {
+  'user.id': {
+    immediate: true,
+    handler(newId) {
+      // Se "selectedUser" mudar de ID, atualiza formData
+      this.formData.id = newId
       this.formData.username = this.user.username
       this.formData.email = this.user.email
       this.formData.password = ''
+    }
+  }
+}
+,
+  methods: {
+    onCancel() {
+      this.$emit('cancel')
     },
-    methods: {
-      onCancel() {
-        // Emite para o pai que cancelou
-        this.$emit('cancel')
-      },
-      async onSave() {
-        try {
-          await userService.updateUser(this.user.id, {
-            username: this.formData.username,
-            email: this.formData.email,
-            password: this.formData.password
-          })
-          // Emite para o pai que salvou
-          this.$emit('saved')
-        } catch (error) {
-          console.error('Erro ao atualizar usuário:', error)
-          // Aqui você pode exibir alguma notificação de erro
-        }
+    async onSave() {
+      try {
+        console.log('Atualizando utilizador ID:', this.formData.id)
+
+        await userService.updateUser(this.formData.id, {
+          username: this.formData.username,
+          email: this.formData.email,
+          password: this.formData.password
+        })
+
+        // Limpar o campo da senha
+        this.formData.password = ''
+
+        // Emite para o pai que salvou
+        this.$emit('saved')
+      } catch (error) {
+        console.error('Erro ao atualizar usuário:', error)
+        // Aqui podes exibir um snackbar ou alerta
       }
     }
   }
-  </script>
-  
+}
+</script>
