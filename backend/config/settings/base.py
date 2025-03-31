2b7f0f2b55b84598e1a8c5f228667093e9bacd2d
DavidK07442 <a81085@ualg.pt>
2025-03-31 17:56:49 +0100

    .

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -234,7 +234,7 @@ CSRF_TRUSTED_ORIGINS = env.list(
         "http://127.0.0.1:3000",
         "http://0.0.0.0:3000",
         "http://localhost:3000",
-        "http://192.168.1.117:3000"
+        "http://10.0.2.15:3000"
     ],
 )
 
@@ -243,7 +243,7 @@ ALLOWED_HOSTS = ["*"]
 
 if DEBUG:
     CORS_ORIGIN_ALLOW_ALL = True
-    CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:3000", "http://0.0.0.0:3000", "http://localhost:3000", "http://192.168.1.117:3000/"]
+    CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:3000", "http://0.0.0.0:3000", "http://localhost:3000", "http://10.0.2.15:3000"]
     CSRF_TRUSTED_ORIGINS += env.list("CSRF_TRUSTED_ORIGINS", [])
 
 # Batch size for importing data
